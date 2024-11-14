4484c9837a6353fd469dbb6fc2084c392bcb3fcf
Jarl Vesterholm Jensen <jarlvjensen99@gmail.com>
2024-11-14 09:17:20 +0100

    add run files + changes to grid_problem and visualize_wfc

--- grid_problem.py
+++ grid_problem.py
@@ -99,62 +99,54 @@ class TileProperties(FuncFit):
         return self.output_data.shape
 
 # %%
-test_grid = np.array(Image.open("images/piskel_example1.png.png"))[..., :3]
-samples_dir = 'images'
-images = [np.array(Image.open(os.path.join(samples_dir, file)))[..., :3] for file in os.listdir(samples_dir)
-          if file.startswith(('piskel'))]
 
-algo = algorithm.NEAT(
-    pop_size=1000,  # Population size
-    species_size=20,  # Size of species
-    survival_threshold=0.1,  # Threshold for survival
-    genome=genome.DefaultGenome(
-        num_inputs=2,  # Normalized Pixel Coordinates and Number of input features (RGB values)
-        num_outputs=4,  # Number of output categories (Red, Brown, Green, Blue)
-        output_transform=common.ACT.sigmoid,  # Activation function for output layer
-    ),
-)
+def cppn_neat(input_grid: np.array, pop_size: int = 1000, species_size: int = 20
+              , survival_threshold: float = 0.1, generation_limit: int = 200
+              , fitness_target: float = -1e-6, seed: int= 42, show_network: bool = False):
+    
+    algo = algorithm.NEAT(
+        pop_size=pop_size,  # Population size
+        species_size=species_size,  # Size of species
+        survival_threshold=survival_threshold,  # Threshold for survival
+        genome=genome.DefaultGenome(
+            num_inputs=2,  # Normalized Pixel Coordinates and Number of input features (RGB values)
+            num_outputs=4,  # Number of output categories (Red, Brown, Green, Blue)
+            output_transform=common.ACT.sigmoid,  # Activation function for output layer
+        ),
+    )
 
-problem = TileProperties(test_grid, tile_size=1)
+    problem = TileProperties(input_grid, tile_size=1)
 
-pipeline = Pipeline(
-    algorithm=algo,  # The configured NEAT algorithm
-    problem=problem,  # The problem instance
-    generation_limit=200,  # Maximum generations to run
-    fitness_target=-1e-6,  # Target fitness level
-    seed=42,  # Random seed for reproducibility
-)
+    pipeline = Pipeline(
+        algorithm=algo,  # The configured NEAT algorithm
+        problem=problem,  # The problem instance
+        generation_limit=generation_limit,  # Maximum generations to run
+        fitness_target=fitness_target,  # Target fitness level
+        seed=seed,  # Random seed for reproducibility
+    )
 
+    
+    
+    state = pipeline.setup()
+    # Run the NEAT algorithm until termination
+    state, best = pipeline.auto_run(state)
+    # Display the results of the pipeline run
+    pipeline.show(state, best)
+    
+    if show_network:
+        network = network_dict(algo.genome, state, *best)
+        visualize_labeled(algo.genome,network,["SGM"], rotate=90, save_path="network.svg", with_labels=True)
 
-
-state = pipeline.setup()
-# Run the NEAT algorithm until termination
-state, best = pipeline.auto_run(state)
-# Display the results of the pipeline run
-pipeline.show(state, best)
-# %%
-
-# %%
-#network = algo.genome.network_dict(state, *best)
-#algo.genome.visualize(network, save_path="images/tileproperties_network.png")
-
-
-network = network_dict(algo.genome, state, *best)
-visualize_labeled(algo.genome,network,["SGM"], rotate=90, save_path="network.svg", with_labels=True)
-# %%
-
-result = vmap(algo.forward,in_axes=(None,None,0))(state, algo.transform(state, best), problem.inputs)
-# %%
-print(result)
-result = np.argmax(result, axis=1)
-print(result)
-result = result.reshape(test_grid.shape[:2])
-print(result)
-
-# %% get ratio of elements
-unique, counts = np.unique(result, return_counts=True)
-print(unique, counts)
-print("----------")
-target = np.argmax(problem.output_data, axis=1)
-unique, counts = np.unique(target, return_counts=True)
-print(unique, counts)
+    algo_forward = vmap(algo.forward,in_axes=(None,None,0))(state, algo.transform(state, best), problem.inputs)
+    result = np.argmax(algo_forward, axis=1)
+    # print(result)
+    # result = result.reshape(test_grid.shape[:2])
+    # print(result)
+    
+    # unique, counts = np.unique(result, return_counts=True)
+    # print(unique, counts)
+    # print("----------")
+    # target = np.argmax(problem.output_data, axis=1)
+    # unique, counts = np.unique(target, return_counts=True)
+    # print(unique, counts)
+    return result
